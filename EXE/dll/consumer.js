46466aa967cea9e8a24aeb569b091cf0896469a5
uldash <uldashevue@gmail>
2019-10-01 23:25:34 +0300

    Revert "add EXE folder"
    This reverts commit 4e228fb064b0ab42b13383d9ae4b7f480f9377ee.

--- EXE/dll/consumer.js
+++
@@ -1,4 +0,0 @@
-// jsc consumer.js
-import LibHello;
-var h = new LibHello.Hello();
-print(h.say());
--- EXE/exe/myapps.js
+++
@@ -1,3 +0,0 @@
-var d = new Date();
-var n = Math.random();
-print('Hellow, \ntoday is ' + d + '\nand this is random - ' + n);
