381eeef70cd936ed12565ef1d107837064f7b242
uldash <uldashevue@gmail.com>
2021-01-02 20:24:50 +0300

    bootstrap bug

---
+++ SITE/06 Online Tutorials/02 Live Character Counter/js/main.js
@@ -0,0 +1,10 @@
+const textarea = document.querySelector("textarea");
+const count = document.querySelector(".count");
+
+function countLetters() {
+    const text = textarea.value;
+    const textLength = textarea.value.length;
+    count.innerHTML = `${textLength}`
+}
+
+textarea.onkeyup = countLetters;
